7e50e840ee7e3c47ae8d1fb7cd552b3c2d744b22
GinoGallardo <ginosgt@gmail.com>
2024-12-19 16:37:19 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        moderateViolet: 'hsl(263, 55%, 52%)',
+        darkGrayishBlue: 'hsl(217, 19%, 35%)',
+        darkBlackishBlue: 'hsl(219, 29%, 14%)',
+        pureWhite: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(0, 0%, 81%)',
+        lightGrayishBlue: 'hsl(210, 46%, 95%)'
+      },
+      fontFamily: {
+        barlow: ['"Barlow Semi Condensed"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
